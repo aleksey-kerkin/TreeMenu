29ecd18fdd2926ca7ec8aa5a5be8be972fff786e
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-09-29 22:29:25 +0500

    Add views, urls for simple test pages

--- tree_menu/views.py
+++ tree_menu/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "tree_menu/home.html")
+
+
+def about(request):
+    return render(request, "tree_menu/about.html")
