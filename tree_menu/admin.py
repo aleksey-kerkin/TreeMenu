135cfd7dda2a76f1453982e44c7aa2b1686b3a3e
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-09-29 22:18:16 +0500

    Register models in admin.py

--- tree_menu/admin.py
+++ tree_menu/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Menu, MenuItem
+
+
+class MenuItemInline(admin.TabularInline):
+    model = MenuItem
+    extra = 1
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug")
+    inlines = [MenuItemInline]
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "menu", "parent", "url", "named_url")
+    list_filter = ("menu",)
